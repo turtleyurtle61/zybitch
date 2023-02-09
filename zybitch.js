e0d1e28297b4d7af918d5f89a91e124f55e15361
turtleyurtle61 <50433311+turtleyurtle61@users.noreply.github.com>
2023-02-08 22:47:44 -0700

    Updated zybitch
    Autocomplete works mostly

--- zybitch.js
+++ zybitch.js
@@ -18,42 +18,81 @@ var autoRun = false;
 
 
 //EDIT ANY BUGS YOU SEE
+
+
+
 (function() {
-    console.log(timeString() + " Autocomplete go brrrrrrrr");
-    if (autoRun) {
+  	function runAutocomplete(zEvent){
+        console.log(timeString() + " [zBA] Running...");
+        click_answers();
+    //     click_options();
+    //     fill_short_answers();
+//         start_activity();
         run();
-    } else {
-        document.getElementsByClassName('right-buttons')[0].innerHTML = '<button id="zbaButton" type="button" style="background:#ff9547;border:1px solid #ffffff;color:#ffffff;padding:8px;border-radius:5px;cursor:pointer">Autocomplete</button>' + document.getElementsByClassName('right-buttons')[0].innerHTML;
-        document.getElementById("zbaButton").addEventListener ("click", zBAStartButton, false);
     }
+  
+    console.log(timeString() + " Autocomplete script loaded successfully");
+  	let toolbar = document.querySelector(".top-toolbar ");
+  	toolbar.insertAdjacentHTML("beforeend", 
+                               '<button id="zbaButton" type="button" onclick="runAutocomplete()" style="background:#ff9547;border:1px solid #ffffff;color:#ffffff;padding:8px;border-radius:5px;cursor:pointer">Autocomplete</button>');
+  	document.getElementById("zbaButton").addEventListener ("click", runAutocomplete, false);
 })();
 
 
-function zBAStartButton (zEvent) {
-    console.log(timeString() + " [zBA] Running...");
-    //click_answers();
-    click_options();
-    fill_short_answers();
-    start_activity();
-    run();
-}
+
+
 
 function run() {
-    //click_speeds();
+    click_speeds();
     click_plays();
     click_starts();
-    setTimeout(function(){ run(); }, 1000);
+    setInterval(() => { 
+      click_plays();
+    }, 2000);
 }
 
 function click_speeds() { // Checks speed boxes. Doesn't work but isn't a necessary feature.
-    var speed = document.getElementsByClassName("speed-control");
-    for (var i = 0; i < speed.length; i++) {
-        if ((speed[i].innerHTML).includes("false")) {
-            //speed[i].click();
-            speed[i].getElementsByClassName("zb-checkbox")[0].innerHTML = "\n<input type=\"checkbox\" value=\"true\" aria-label=\"2x speed\">\n<label aria-hidden=\"true\">2x speed</label>\n"
-            console.log(timeString() + " Checked a speed box.");
-        }
+    let speeds = document.getElementsByTagName("input");
+  
+//  		const timer = ms => new Promise(res => setTimeout(res, ms))
+//     async function load () { // We need to wrap the loop into an async function for this to work
+//       for (var i = 0; i<speeds.length; i++) {
+//         if(speeds[i].type.toLowerCase() == 'checkbox'){
+//           console.log(speeds[i]);
+//           speeds[i].value = true;
+//           speeds[i].click();
+//         }
+//         console.log("async working or naw??")
+//         await timer(50000); // then the created Promise can be awaited
+//       }
+//     }
+
+//     load();
+  	var i = 0;                  //  set your counter to 1
+
+    function myLoop() {         //  create a loop function
+      setTimeout(function() {   //  call a 3s setTimeout when the loop is called
+        console.log('hello');   //  your code here
+        i++;                    //  increment the counter
+        if (i<speeds.length){           //  if the counter < 10, call the loop function
+          console.log(speeds[i]);
+          speeds[i].value = true;
+          speeds[i].click();
+          myLoop();             //  ..  again which will trigger another 
+        }                       //  ..  setTimeout()
+      }, 300)
     }
+  
+  	myLoop();
+
+//   	for(let i=0; i<speeds.length; i++){
+//       if(speeds[i].type.toLowerCase() == 'checkbox'){
+//         console.log(speeds[i]);
+//         speeds[i].value = true;
+//         speeds[i].click();
+//       }
+//     }
+ 		console.log(speeds);
 }
 
 function click_plays() { // Clicks all Play buttons
@@ -68,61 +107,65 @@ function click_plays() { // Clicks all Play buttons
 
 
 
-//IN PROGRESS
-function click_answers(){ //clicks check answer
-    var answers = document.getElementsByClassName("show-answer-button");
-    for(var i=0; i<answers.length; i++){
+// //IN PROGRESS
+function click_answers(){ // clicks all mc options
+    let answers = document.getElementsByTagName("input");
+
+  	for(let i=0; i<answers.length; i++){
+      if(answers[i].type.toLowerCase() == 'radio'){
         answers[i].click();
-        answers[i].click();
-        console.log("clicked answer button twice.");
+        
+      }
     }
+//  		console.log(answers);
 }
 
 
-function click_options(){ //click all MC options
-    var options = document.querySelectorAll('input[type=radio]');
-    for(var i=0; i<options.length; i++){
-        options[i].click();
-        console.log("clicked options button.");
-    }
-}
+// function click_options(){ //click all MC options
+//     var options = document.querySelectorAll('input[type=radio]');
+//     for(var i=0; i<options.length; i++){
+//         options[i].click();
+//         console.log("clicked options button.");
+//     }
+// }
 
 
-function fill_short_answers(){ //fills in short answer
-    var answer_div = document.getElementsByClassName("short-answer-question");
-    var answer;
-    var input;
-    var check;
-    click_answers();
+// function fill_short_answers(){ //fills in short answer
+//     var answer_div = document.getElementsByClassName("short-answer-question");
+//     var answer;
+//     var input;
+//     var check;
+//     click_answers();
 
-    for(var i=0; i<answer_div.length; i++){
-        answer = answer_div[i].getElementsByClassName("forfeit")[0].getElementsByClassName("forfeit-answer")[0].innerText;
-        input = answer_div[i].getElementsByTagName("textarea")[0];
-        input.value = answer;
-        console.log(input);
-    }
-}
-
-
-function start_activity(){ //Starts those multiple part look at code and fill in expected answer questions
-    var start_button = document.getElementsByClassName("zyante-progression-start-button");
-    if(start_button != undefined){
-        for(var i=0; i<start_button.length; i++){
-            start_button[i].click();
-        }
-    } else {
-        console.log("No activities to start!");
-    }
-}
-//^^IN PROGRESS
+//     for(var i=0; i<answer_div.length; i++){
+//         answer = answer_div[i].getElementsByClassName("forfeit")[0].getElementsByClassName("forfeit-answer")[0].innerText;
+//         input = answer_div[i].getElementsByTagName("textarea")[0];
+//         input.value = answer;
+//         console.log(input);
+//     }
+// }
 
 
+// function start_activity(){ //Starts those multiple part look at code and fill in expected answer questions
+//     var start_button = document.getElementsByClassName("zyante-progression-start-button");
+//     if(start_button != undefined){
+//         for(var i=0; i<start_button.length; i++){
+//             start_button[i].click();
+//         }
+//     } else {
+//         console.log("No activities to start!");
+//     }
+// }
+// //^^IN PROGRESS
 
-function click_starts() { // Clicks all Start buttons
+
+
+function click_starts(){ // Clicks all Start buttons
     var starts = document.getElementsByClassName("start-button");
     for (var i = 0; i < starts.length; i++) {
         starts[i].click();
-        console.log(timeString() + " Clicked a start button.");
+      	console.log(starts[i]);
+//         console.log(timeString() + " Clicked a start button.");
     }
 }
 
